604f3bb2478fe54771b61dddd3bc8d5a6be2b88a
Jeff <11721615+jeffellin@users.noreply.github.com>
2021-05-04 12:57:37 -0400

    add comment

--- src/main/java/com/example/demo/customer/Customer.java
+++ src/main/java/com/example/demo/customer/Customer.java
@@ -16,7 +16,14 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    /**
+     * Area codes start with a number 2–9, followed by 0–8, and then any third digit.
+     *
+     * The second group of three digits, known as the central office or exchange code, starts with a number 2–9,
+     * followed by any two digits.
+     *
+     * The final four digits, known as the station code, have no restrictions.
+     */
     @Pattern(  message = "{BranchCode.customer.branch}", regexp = "^\\(?([2-9][0-8][0-9])\\)?[-. ]?([2-9][0-9]{2})[-. ]?([0-9]{4})$")
     @Phone(id="foo")
     private String phoneNumber;
